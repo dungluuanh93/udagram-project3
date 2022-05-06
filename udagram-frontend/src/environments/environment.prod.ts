7378ab5e6e40663a5c901d8452babf8eb37d6172
dungluuanh93 <dungluuanh93@gmail.com>
2022-05-06 15:38:26 +0700

    fix wrong POSTGRES variable

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac1d4d85735af45aa9e8b7bf438ad1f0-222049689.ap-southeast-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
